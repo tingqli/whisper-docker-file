ec7365e0d147f88d8fbfe51a54761421a89c660a
Li, Tingqian <tingqli@amd.com>
2025-10-15 23:11:12 +0800

    torch2.8.0 & zh & faster-whisper

--- test-accuracy.py
+++ test-accuracy.py
@@ -7,6 +7,8 @@ import glob, librosa
 from tqdm import tqdm
 import numpy as np
 
+with_profiler = os.environ.get("VLLM_TORCH_PROFILER_DIR", None) is not None
+
 model_id = "whisper-large-v3"
 
 def speech_recognition_hf(dataset):
@@ -38,6 +40,22 @@ def speech_recognition_hf(dataset):
 
     return predictions
 
+def speech_recognition_faster_whisper(dataset, beam_size = 1):
+    from faster_whisper import WhisperModel, BatchedInferencePipeline
+    model_size = "large-v3"
+    model = WhisperModel(model_size, device="cuda", compute_type="float16")
+    batched_model = BatchedInferencePipeline(model=model)
+    predictions = []
+    for i in tqdm(range(dataset.num_rows)):
+        audio = dataset[i]["audio"]["array"]
+        language = dataset[i]["language"]
+        # segments, info = batched_model.transcribe(audio=audio, batch_size=8, beam_size=beam_size, without_timestamps=True, language=language)
+        segments, info = model.transcribe(audio=audio, beam_size=beam_size, without_timestamps=True, language=language)
+        # print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+        segments = list(segments)
+        assert len(segments) == 1
+        predictions.append(segments[0].text)
+    return predictions
 
 def speech_recognition_vllm(dataset, batch_size = 1, max_tokens = 500, use_fp8_kv_cache=False, enforce_eager=False):
     from vllm import LLM, SamplingParams    
@@ -72,9 +90,11 @@ def speech_recognition_vllm(dataset, batch_size = 1, max_tokens = 500, use_fp8_k
     )
     import time
     #llm.start_profile()
+    if with_profiler: llm.start_profile()
     start = time.time()
     outputs = llm.generate(prompts,sampling_params)
     duration = time.time() - start
+    if with_profiler: llm.stop_profile()
     #llm.stop_profile()
     print(f"len(prompts):{len(prompts)}")
     print("Duration:", duration)
@@ -95,10 +115,10 @@ def load_1000_datasets():
         if not isinstance(text, str):
             print(f">>>>>>>>> skip {row['aid']} with no expected answer")
         else:
-            audio_files = glob.glob(os.path.join(wav_path, f'*{row['aid']}*.wav'))
+            audio_files = glob.glob(os.path.join(wav_path, f'*{row["aid"]}*.wav'))
             assert len(audio_files) == 1
             audio_file = audio_files[0]
-            audio, sample_rate = librosa.load(audio_file,sr=None)
+            audio, sample_rate = librosa.load(audio_file,sr=None, dtype=np.float32)
             if sample_rate != sr:
                 audio = librosa.resample(audio.numpy().astype(np.float32), orig_sr=sample_rate, target_sr=sr)
                 sample_rate = sr
@@ -139,6 +159,14 @@ def load_librispeech_asr_test_datasets():
         return row
     return dataset.map(preprocess)
 
+def load_WenetSpeech_datasets():
+    dataset = load_dataset("TwinkStart/WenetSpeech", split="test_meeting", trust_remote_code=True)
+    def preprocess(row):
+        row["language"] = "zh"
+        return row
+    return dataset.map(preprocess)
+
+
 if __name__ == "__main__":
     # https://huggingface.co/docs/datasets/access
     #dataset = load_demo_datasets()
@@ -146,22 +174,38 @@ if __name__ == "__main__":
     #dataset = load_1000_datasets()
 
     # HF wer_score: 1.74 %
-    # Accuracy is consistent in both AMD & NV
     # vLLM wer_score batch-size1  ,kv-cache-fp16: 1.88 %
     # vLLM wer_score batch-size1  ,kv-cache-fp8 : 1.88 %
     # vLLM wer_score batch-size256,kv-cache-fp8 : 1.88 %
-    dataset = load_librispeech_asr_test_datasets()
+    #
+    # faster_whisper beam_size=1: wer_score: 1.79 %  5.05it/s
+    # faster_whisper beam_size=5: wer_score: 1.74 %  4.52it/s
+    #dataset = load_librispeech_asr_test_datasets()
+
+    # faster_whisper beam_size=1: wer_score:  
+    dataset = load_WenetSpeech_datasets()
     dataset = dataset.filter(lambda example: example["audio"]["array"].shape[0] < 30*16000)
+    dataset = dataset.select(range(0,20))
 
     print(dataset)
 
     #predictions = speech_recognition_hf(dataset)
     predictions = speech_recognition_vllm(dataset, use_fp8_kv_cache=True, batch_size=256)
+    #predictions = speech_recognition_faster_whisper(dataset, beam_size=5)
+
     # https://pypi.org/project/whisper-normalizer/
     from whisper_normalizer.english import EnglishTextNormalizer
-    english_normalizer = EnglishTextNormalizer()
-    predictions = [english_normalizer(x) for x in predictions]
-    references = [english_normalizer(dataset[i]["text"]) for i in range(dataset.num_rows)]
+    normalizer = EnglishTextNormalizer()
+    def ZhTextNormalizer(x): return x
+    normalizer = ZhTextNormalizer
+    predictions = [normalizer(x) for x in predictions]
+    references = [normalizer(dataset[i]["text"]) for i in range(dataset.num_rows)]
+
+    if 0:
+        for p, r, in zip(predictions, references):
+            print("==========")
+            print(r)
+            print(p)
 
     import evaluate
     wer = evaluate.load("wer")
